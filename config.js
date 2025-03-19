2ec6ce294a332718cbf47c4dc231429ef4b38a6a
Vinnie <vmatronia@gmail.com>
2025-03-18 23:19:41 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,65 @@
+/* Amazon Link */
+
+// Base URL
+var URL_SITE = "https://coolv1994.github.io/Amazon";
+
+// Default Tag
+var TAG_DEFAULT = "gawdtech-20";
+
+
+// Class: Hidden
+var CLASS_HIDDEN = "hide";
+
+
+// Section: Redirect
+var SECTION_REDIRECT = "redirect";
+
+// Hyperlink: Redirect
+var LINK_REDIRECT = "url-redirect";
+
+
+// Section: Link
+var SECTION_LINK= "link";
+
+// Section: Link
+var SECTION_LINK_NEW= "new";
+
+// Section: Link
+var SECTION_LINK_OLD= "old";
+
+// Section: Link
+var TITLE_NEW= "new-title";
+
+// Section: Link
+var TITLE_OLD= "old-title";
+
+// Text Box: Affiliate
+var INPUT_URL_NEW = "url-new";
+
+// Text Area: Amazon
+var INPUT_URL = "url-old";
+
+// Text: Status Message
+var TEXT_MESSAGE = "message";
+
+// Button: Generate
+var BUTTON_LINK = "generate";
+
+// Button: Clear Input
+var BUTTON_CLEAR = "clear";
+
+// Button: Copy Text
+var BUTTON_COPY = "copy";
+
+
+// Section: List
+var SECTION_LIST = "list";
+
+// Text: Title
+var LIST_TITLE = "list-title";
+
+// Text: Info
+var LIST_INFO = "list-info";
+
+// List: Items
+var LIST_ITEMS = "list-items";
